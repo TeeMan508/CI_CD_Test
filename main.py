2e9a4491ef37459ea93c7f7ba297fe647623e792
TMN <super.rikunen@gmail.com>
2024-09-18 14:06:57 +0300

    Fix 25

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-import logging
-import time
-
 from bin import calculate_mean
 
 
 def main_func():
-    logging.info("123321")
     raise AssertionError("ERROR HERE!!!")
 
 
--- tests/mean/test_mean.py
+++ tests/mean/test_mean.py
@@ -6,8 +6,8 @@ def test_mean():
     assert abs(calculate_mean([1, 2, 3]) - 2.) < 1e-6
 
 
-# def test_launch_ability() -> None:
-#     try:
-#         main_func()
-#     except Exception as e:
-#         assert e is None
+def test_launch_ability() -> None:
+    try:
+        main_func()
+    except Exception as e:
+        assert e is None

fbfc658f823cf02618cf6da87f2392e5fb95828d
TMN <super.rikunen@gmail.com>
2024-09-18 13:29:32 +0300

    Fix 23

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
+import time
+
 from bin import calculate_mean
 
 
 #!/opt/hostedtoolcache/Python/3.11.9/x64
 
 def main_func():
-    raise AssertionError("ERROR HERE!!!")
+    while True:
+        time.sleep(1)
+    # raise AssertionError("ERROR HERE!!!")
 
 
 if __name__ == "__main__":

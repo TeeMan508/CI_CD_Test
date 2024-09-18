82ad841daefcfa9e6c5cb16e420dba99b60d849a
TMN <super.rikunen@gmail.com>
2024-09-18 11:38:59 +0300

    Fix 19

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from bin import calculate_mean
 
 
+# ! /opt/hostedtoolcache/Python/
+
 def main_func():
     raise AssertionError("ERROR HERE!!!")
 
+
 if __name__ == "__main__":
     main_func()
-
